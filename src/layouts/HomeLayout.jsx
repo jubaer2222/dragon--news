db57adb0dd7766b30bd31a9c0e32637785139b45
jubaer2222 <139365401+jubaer2222@users.noreply.github.com>
2025-05-05 23:00:57 +0600

    add few features

--- src/layouts/HomeLayout.jsx
+++ src/layouts/HomeLayout.jsx
@@ -1,18 +1,20 @@
 import React from 'react';
-import { Outlet } from 'react-router';
+import { Outlet, useNavigation } from 'react-router';
 import Header from '../components/Header';
 import LatestNews from '../components/LatestNews';
 import Navbar from '../components/Navbar';
 import LeftAside from '../components/homelayout/LeftAside';
 import RightAside from '../components/homelayout/RightAside';
+import Loading from '../pages/loading';
 
 
 const HomeLayout = () => {
+    const {state} = useNavigation();
     return (
         <div>
             <header>
                 <Header></Header>
-
+                 {import.meta.env.VITE_name}
                <section className='w-11/12 mx-auto my-3'>
                <LatestNews></LatestNews>
                </section>
@@ -28,7 +30,7 @@ const HomeLayout = () => {
                 </aside>
 
                 <section className="main col-span-6 mx-7">
-                    <Outlet></Outlet>
+                   {state == "loading" ? <Loading/> : <Outlet></Outlet>} 
                 </section>
                 
                 <aside className='col-span-3 sticky top-0 h-fit'>
--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -4,6 +4,12 @@ import LatestNews from "../components/LatestNews";
 import HomeLayout from "../layouts/HomeLayout";
 import Home from "../pages/Home";
 import CategoryNews from "../pages/CategoryNews";
+import Register from "../pages/Register";
+import Login from "../pages/Login";
+import AuthLayout from "../layouts/AuthLayout";
+import NewsDetails from "../pages/NewsDetails";
+import PrivateRoute from "../provider/PrivateRoute";
+import Loading from "../pages/loading";
 
 const router = createBrowserRouter([
     {
@@ -17,14 +23,33 @@ const router = createBrowserRouter([
             {
                 path:"/category/:id",
                 element:<CategoryNews></CategoryNews>,
-                loader:()=> fetch("/news.json")
+                loader:()=> fetch("/news.json"),
+                hydrateFallbackElement:<Loading></Loading>
+            }
+        ]
+    },
+    {
+        path:"/auth",
+        element:<AuthLayout></AuthLayout>,
+        children:[
+            {
+                path:"/auth/login",
+                element:<Login></Login>
+            },
+            {
+                path:"/auth/register",
+                element:<Register></Register>
             }
         ]
     },
    
     {
-        path: "/news",
-        element: <h2>News Layout</h2>
+        path: "/news-details/:id",
+        element: (<PrivateRoute>
+            <NewsDetails></NewsDetails>
+        </PrivateRoute>),
+        loader: ()=>fetch("/news.json"),
+        hydrateFallbackElement:<Loading></Loading>
     },
     {
         path: "/*",
